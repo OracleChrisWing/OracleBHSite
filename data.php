84fbcbf29c4318ad8b156ac7120184f5ba860271
benscott1994 <ben.scott94@hotmail.com>
2018-02-19 11:31:57 +0000

    Add files via upload

--- data.php
+++ data.php
@@ -1,27 +1,32 @@
-<hmtl>
+<html>
 	<head>
+        
 		<!-- self -->
 		<?php include("header.php"); ?>
-		<link rel="stylesheet" href="CSS/data.css"/>
 	</head>
-	<body>
-		<div id="content">
+			<div id="content">
 			<?php include("nav.php"); ?>
 			<div id="return">
 				<a href="./">
 					<img src="images/return-icon.png"></img>Return to landing page...
 				</a>
 			</div>
+	<body>
+		<div id="content">
+			<?php include("nav.php"); ?>
 			<br>
-			<div class="main_window">
-				<div class="sec1">
-					<div class="graph">
-					
-					</div>
-				</div>
-				<div class="sec2">
-					<div class="graph">
-					
+
+            	<a name="image"></a>
+                            <section class="image">
+							<img width="24%" height="90%" img align="right" src="https://www.theglobeandmail.com/resizer/-4ECuLzX_PpkKoMgf7xUP344rFw=/1200x0/filters:quality(80)/arc-anglerfish-tgam-prod-tgam.s3.amazonaws.com/public/PMQSOZENMBGF3D5IEFNKMIR7YY"></img>
+            
+							<iframe id="dataVisualFrm" width="75%" height="90%" src="http://130.61.25.101:9704/va/project.jsp?pageid=visualAnalyzer&reportmode=presentation&reportpath=%2Fshared%2FBloodhound%2FPublic%2FDEMO%20%5C*Bloodhound%20Sensors%20Mobile%20copy%5C*"></iframe>
+
+
+
+						
+						</td>
+						</div>
 					</div>
 				</div>
 			</div>
